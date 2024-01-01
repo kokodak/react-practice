b0f1cd12d7f99fda9d0fa6ef896705c6bbe43bcd
kokodak <kokodakadokok@gmail.com>
2024-01-01 21:45:25 +0900

    간단한 TODO 만들기

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,14 +1,43 @@
 import React, { useState } from "react"
 
-function LikeButton() {
-  const [liked, setLiked] = useState(false)
-  if (liked) return 'you liked this';
-  return React.createElement(
-    'button',
-    {
-      onClick: () => setLiked(true)
-    }
+function App() {
+  const [todo, setTodo] = useState([]);
+  const [newTodo, setNewTodo] = useState('');
+
+  const onChangeNewTodo = e => {
+    setNewTodo(e.target.value)
+  }
+
+  const onSubmit = e => {
+    e.preventDefault();
+    const nextTodo = [...todo, {title: newTodo, completed: false, id: Math.random()}];
+    setTodo(nextTodo);
+  }
+
+  return (
+    <> {/* Fragment */}
+      <div className="App">TODO</div>
+      <div className="App">
+        {todo.length === 0 ? (
+          <div>
+            <div>할 일을 추가해보세요.</div>
+            <form onSubmit={onSubmit}>
+              <input value={newTodo} onChange={onChangeNewTodo}/>
+              <button>추가</button>
+            </form>
+            
+          </div>
+        ) : <>
+              {todo.map(t => (<div key={t.id}>{t.title}</div>))}
+              <form onSubmit={onSubmit}>
+                <input value={newTodo} onChange={onChangeNewTodo}/>
+                <button>추가</button>
+              </form>
+            </>
+        }
+      </div>
+    </>
   )
 }
 
-export default LikeButton
+export default App
--- vite-project/src/main.tsx
+++ vite-project/src/main.tsx
@@ -1,22 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import LikeButton from './App.tsx'
+import App from './App.tsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <LikeButton />
-  </React.StrictMode>,
-)
-
-ReactDOM.createRoot(document.getElementById('root-2')!).render(
-  <React.StrictMode>
-    <LikeButton />
-  </React.StrictMode>,
-)
-
-ReactDOM.createRoot(document.getElementById('root-3')!).render(
-  <React.StrictMode>
-    <LikeButton />
-  </React.StrictMode>,
+    <App />
 )
